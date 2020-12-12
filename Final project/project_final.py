44a5ccfc8a24565515206b56e1e884fe6c45dc21
Andriy Osemchuk <andriyosemchuk@gmail.com>
2020-12-12 23:37:15 +0200

    final project read all files into dataframe

---
+++ Final project/project_final.py
@@ -0,0 +1,27 @@
+
+# import libs
+import pandas as pd
+import numpy as np
+import glob
+
+path = 'data/*'
+folder = ['idle/', 'running/', 'stairs/', 'walking/']
+
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+# print(all_files)
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+
+#for i in folder:
+ #   all_files = glob.glob(path + i + "*.csv")
+  #  print(all_files)
+
+#df = pd.read_csv('data/idle/idle-1.csv')
+#df.count()
